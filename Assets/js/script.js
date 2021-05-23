c164c7c284c9475e0eaa3e9815ac1d5221d198b6
drewml08 <drewml08@gmail.com>
2021-05-22 23:51:30 -0400

    Added style changes, added timer and high score functions, as well as game over logic.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,9 +10,18 @@ var questions = [
     { title: "The _______ method of an Array object adds and/or removes elements from an array.", a: "Reverse", b: "Shift", c: "Slice", d: "Splice", correct: "d", score: 5},
 ];
 
+var clockInterval = null;
+var secondsLeft = 0;
+var highscores = JSON.parse( localStorage.getItem( "week4-highscores" ) );
 var current = 0;
+var totalscore = 0;
 
 function displayQuestion (index) {
+    if (index >= questions.length){
+        gameOver();
+        return;
+    }
+
     var question = document.querySelector("#prompt");
     question.innerHTML = questions[index].title;
 
@@ -38,14 +47,125 @@ function answerQuestion (answer) {
     var response = document.querySelector("#response");
     if (answer === questions[current].correct){
         response.innerHTML = "Correct!";
+        totalscore += questions[current].score;
     } else {
         response.innerHTML = "Wrong Answer!";
+        secondsLeft -= 30;
     }
     var timerInterval = setInterval(function() {
         clearInterval(timerInterval);
         displayQuestion (current + 1);
     
       }, 2500);
+}
+
+function updateClock() {
+    var clock = document.querySelector( "#clock" );
+    clock.innerHTML = new Date( secondsLeft * 1000 ).toISOString().substr( 11, 8 );    
+}
+
+function startClock() {
+    secondsLeft = 5 * 60;
+    updateClock();
+
+    clockInterval = setInterval( function() {
+        --secondsLeft;
+        updateClock();
+        if (secondsLeft === 0) {
+            gameOver();
+        }
+
+    }, 1000 );
+}
+
+function stopClock() {
+    clearInterval( clockInterval );
+}
+
+function gameOver()
+{
+    stopClock();
+
+    var summary = document.querySelector("#summary");
+    summary.innerHTML = "Your Score is: " + totalscore;
+
+    if( secondsLeft === 0 ) {
+        // Game is over because time ran out
+    }
+    hidecard("#start");
+    hidecard("#question");
+    showcard("#gameover");
+    hidecard("#highscores");
+}
+
+function sortByScore( a, b )
+{
+    if( a.score < b.score ) {
+        return 1;
+    }
+    else if( a.score > b.score ) {
+        return -1;
+    }
+    return 0;
+}
+
+function clearChildren( parent ) {
+    if( parent === null ) return;
+
+    var node = parent.firstChild;
+    while( node ) {
+        var remove = node;
+        node = node.nextSibling;
+        parent.removeChild( remove );
+    }
+}
+
+function addHighscore( score ) {
+    if( highscores === null ) {
+        highscores = [];
+    }
+    highscores.push( score );
+    highscores.sort( sortByScore );
+    localStorage.setItem( "week4-highscores", JSON.stringify( highscores ) );
+}
+
+function clearHighscores() {
+    highscores = [];
+    localStorage.removeItem( "week4-highscores" );
+    showHighscores();
+}
+
+function showHighscores() {
+    var divScores = document.querySelector( "#leaderboard" );
+
+    clearChildren( divScores );
+    if( highscores.length === 0 ) {
+        return;
+    }
+
+    var line = document.createElement( "div" );
+    line.classList.add( "score" );
+    line.classList.add( "header" );
+    line.innerHTML = "<p>Name</p><p>Score</p>";
+    divScores.appendChild( line );
+
+    for( var i = 0; i < highscores.length; ++i ) {
+        line = document.createElement( "div" );
+        line.classList.add( "score" );
+        line.innerHTML = "<p>" + highscores[ i ].name + "</p><p>" + highscores[ i ].score + "</p>";
+        divScores.appendChild( line );
+    }
+    hidecard("#start");
+    hidecard("#question");
+    hidecard("#gameover");
+    showcard("#highscores");
+
+}
+
+function submitHighscore() {
+    var playerName = document.querySelector( "#name" );
+    addHighscore({ name: playerName.value, score: totalscore});
+    showHighscores();
 }
 
 function showcard(id) {
@@ -63,6 +183,7 @@ function initializeQuiz() {
     hidecard("#question");
     hidecard("#gameover");
     hidecard("#highscores");
+    totalscore = 0;
 }
 
 function startQuiz() {
@@ -70,6 +191,7 @@ function startQuiz() {
     showcard("#question");
     hidecard("#gameover");
     hidecard("#highscores");
+    startClock();
     displayQuestion(0);
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Week4-hw
+The homework assignment was to build a timed interactive coding quiz using JavaScript, HTML and CSS.  All acceptance criteria was tested and achieved.
+
